89dd4d2670d7ed25546a39745e4cf9fe8941f540
Gabriel Bispo <109290690+gabrielgcb@users.noreply.github.com>
2022-09-13 17:42:50 -0300

    Add files via upload

---
+++ Lista 02/exe24.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+  int main ()
+{
+	float salario, reajuste;
+	scanf("%f", &salario);
+	
+	if(salario <= 300){
+		
+		reajuste = salario*1.5;
+		
+		printf("SALARIO COM REAJUSTE = %.2f\n", reajuste);
+		
+	}
+	
+	else {
+		
+		reajuste = salario*1.3;
+		
+		printf("SALARIO COM REAJUSTE = %.2f\n", reajuste);
+		
+	}
+  
+  return 0;
+  
+}
